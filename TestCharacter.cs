2c1c60ddbe0e398050c15b44228c90380d1a5087
Trevor Grieger <trevorpsn@gmail.com>
2025-07-29 20:38:42 -0400

    Add basic animation for up, down, left, and right

--- TestCharacter.cs
+++ TestCharacter.cs
@@ -5,6 +5,13 @@ public partial class TestCharacter : CharacterBody2D
 	[Export] // Allows you to modify the speed in the editor
 	public float Speed { get; set; } = 200.0f;
 
+	private AnimatedSprite2D _playerAnimatedSprite;
+
+	public override void _Ready()
+	{
+		_playerAnimatedSprite = GetNode<AnimatedSprite2D>("PlayerAnimatedSprite");
+	}
+
 	public override void _PhysicsProcess(double delta)
 	{
 		// Get input direction
@@ -12,6 +19,22 @@ public partial class TestCharacter : CharacterBody2D
 
 		// Set velocity based on input and speed
 		Velocity = inputDirection * Speed;
+		if (inputDirection.Y < 0)
+		{
+			_playerAnimatedSprite.Play("Up");
+		}
+		else if (inputDirection.Y > 0)
+		{
+			_playerAnimatedSprite.Play("Down");
+		}
+		else if (inputDirection.X == 1)
+		{
+			_playerAnimatedSprite.Play("Left");
+		}
+		else if (inputDirection.X == -1)
+		{
+			_playerAnimatedSprite.Play("Right");
+		}
 
 		// Move the character and handle collisions
 		MoveAndSlide();
