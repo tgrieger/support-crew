5fde1898a5e3af765eb17154b3e6ae8b37bbb514
Avenjade <csteinhiser@gmail.com>
2025-08-01 20:29:19 +0900

    Item Timer
    Made a little radial timer for how long is left to provide the next item.

--- HealthControl.cs
+++ HealthControl.cs
@@ -5,17 +5,24 @@ public partial class HealthControl : Control
 	private ProgressBar _healthBar;
 	private Timer _healthTimer;
 	private ItemSprite _objectiveItemSprite;
+	private TextureProgressBar _deliveryTimerBar;
 
 	public override void _Ready()
 	{
 		_healthBar = GetNode<ProgressBar>("HealthBar");
 		_healthTimer = GetNode<Timer>("HealthTimer");
 		_objectiveItemSprite = GetNode<ItemSprite>("ObjectiveItemSprite");
+		_deliveryTimerBar = GetNode<TextureProgressBar>("DeliveryTimerBar");
 
 		_healthTimer.Timeout += OnHealthTimerTimeout;
 
 		// TODO don't hardcode objective
 		_objectiveItemSprite.ItemResource = ResourceLoader.Load<ItemResource>("res://Items/Ammo/ammo.tres");
+	}
+	
+	public override void _Process(double delta)
+	{
+		_deliveryTimerBar.Value = _healthTimer.TimeLeft;
 	}
 
 	private void OnHealthTimerTimeout()
